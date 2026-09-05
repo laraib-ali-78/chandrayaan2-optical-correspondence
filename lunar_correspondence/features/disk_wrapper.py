8084883b9e5ef73d21190859e492b27bda5175cf
laraib-ali-78 <lk583039@gmail.com>
2026-09-06 01:01:09 +0530

    Upload lunar_correspondence/features/disk_wrapper.py for Chandrayaan-2 correspondence pipeline

---
+++ lunar_correspondence/features/disk_wrapper.py
@@ -0,0 +1,11 @@
+"""DISK Feature Extractor Wrapper."""
+
+from typing import Tuple, Optional
+import numpy as np
+from .superpoint_wrapper import SuperPointWrapper
+
+
+class DISKWrapper(SuperPointWrapper):
+    """DISK learned feature extractor interface."""
+    pass
+
